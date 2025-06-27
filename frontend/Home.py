0ae725c025e8272da6bcbf0faca5bc154b0a41dd
lukethegrad <luke@thegraduation.co>
2025-06-27 16:48:02 +0100

    Create Home.py

---
+++ frontend/Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="A&R Tool", layout="centered")
+
+st.title("👋 Welcome to the A&R Discovery Tool")
+st.markdown("Use the sidebar to begin analyzing TikTok sounds.")
